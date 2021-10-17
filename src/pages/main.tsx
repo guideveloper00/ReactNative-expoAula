23eb54dfe5d8056e418fe27d2d46c389825accc0
Guilherme Goncalves <guilherm3goncalves@hotmail.com>
2021-10-17 19:17:49 -0300

    aprendizado reactnative

---
+++ src/pages/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const Main: React.FC = () => {
+    return (
+        <View>
+            <h1>Homenagem aos professores RocketSeat, Yohanes e Grazi</h1>
+        </View>
+    )
+}
+
+export default Main
